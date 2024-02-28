93d80899426b5d7bc01ba214e825172ebcded3d6
RyosukeYashima <r.yashima@yds-net.com>
2024-02-28 19:50:59 +0900

    seconds

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'prototypes/index'
   resources :prototypes 
-  
+  delete 'users/sign_out', to: 'devise/sessions#destroy'
   root to: 'prototypes#index'
 end
