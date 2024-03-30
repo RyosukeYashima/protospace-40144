3fbc8433ebedd4c7f283eea95838ca766285694f
RyosukeYashima <r.yashima@yds-net.com>
2024-03-30 18:25:31 +0900

    バリデーション改正

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  validates :title,:image, presence: true
+  validates :title,:image,:concept,:catch_copy, presence: true
   has_many :comments, dependent: :destroy
 end
